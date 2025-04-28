d3b0882a5b9c4870bf08d05002c41ca397dfeb77
Annurofiq <rofiqcrb75@gmail.com>
2025-04-28 22:43:00 +0700

    Navbar

--- src/components/NavbarComponents.jsx
+++ src/components/NavbarComponents.jsx
@@ -1,17 +1,38 @@
 import { Navbar, Container, Nav } from "react-bootstrap";
 
+import { navLinks } from "../data/index";
+import { NavLink, Link } from "react-router-dom";
+
 const NavbarComponents = () => {
   return (
     <div>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand as={Link} to="/">
+            React-Bootstrap
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
+            <Nav className="mx-auto">
+              {navLinks.map((link) => {
+                return (
+                  <div key={link.id}>
+                    <NavLink to={link.path}>{link.text}</NavLink>
+                  </div>
+                );
+              })}
             </Nav>
+            <Navbar className="bg-body-tertiary">
+              <Container>
+                <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                  <Navbar.Text>
+                    Signed in as: <a href="#login">Mark Otto</a>
+                  </Navbar.Text>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
           </Navbar.Collapse>
         </Container>
       </Navbar>
