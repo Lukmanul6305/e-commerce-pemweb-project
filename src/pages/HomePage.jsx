df83bba145facb5e776feab210d94a773aeb6b5b
ofioktaviani <ofioktaviani110@gmail.com>
2025-05-03 15:32:04 +0700

    kelas section tapi belum di tambahka gambar fiks

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,18 @@
 import { Container, Row, Col } from "react-bootstrap";
 import HeroImage from "../assets/img/hero.png";
 import { kelasTerbaru } from "../data/index";
+import {useNavigate
+
+} from 'react-router-dom';
+
 
 const HomePage = () => {
+  let Navigate = useNavigate();
   return (
     <div className="homepage">
       <header className="w-100 min-vh-100 d-flex align-items-center">
         <Container>
-          <Row className="header-box d-flex align-items-center">
+          <Row className="header-box d-flex align-items-center pt-lg-5">
             <Col lg="6">
               <h1 className="mb-4">
                 Belajar Skill Digital <br /> yang Dibutuhkan Dunia Kerja
@@ -46,13 +51,34 @@ const HomePage = () => {
             {kelasTerbaru.map((kelas) => {
               return (
                 <Col key={kelas.id}>
-                  <img src={kelas.image} alt="unplash.com" />
+                  <img src={kelas.image} alt="unplash.com" className="w-100 mb-5 rounded-top" />
+                  <h5 className="mb-5 px-3">{kelas.title}</h5>
+                  <div className="ket d-flex justify-content-between align-items-center px-3 pb-3">
+                    <p className="m-0 text-primary fw-bold">{kelas.price}</p>
+                    <button className="btn btn-danger rounded-1">{kelas.buy}</button>
+                  </div>
                 </Col>
               );
             })}
           </Row>
+          <Row>
+            <Col className="text-center">
+            <button className="btn btn-success rounded-5 btn-lg" onClick={() => Navigate("/kelas")}>
+              Lihat Semua Kelas</button>
+            <i className="fa-solid fa-chevron-right ms-3"></i>
+            </Col>
+          </Row>
         </Container>
       </div>
+      <div className="testinmonial py-5">
+        <Container>
+          <Row>
+            <Col>
+            <h1 className="text-center fw-bold my-5">Testimonial</h1></Col>
+          </Row>
+        </Container>
+      </div>
+
     </div>
   );
 };
