f431c9f9a8b28d9691bebf834e96f1377401963b
Annurofiq <rofiqcrb75@gmail.com>
2025-05-07 00:08:20 +0700

    memperbaiki tampilan pas scroll kebawah navbar tidak muncul dan mulai awalan footer

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,5 +1,37 @@
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <div className="footer py-5">
+      <Container>
+        <Row>
+          <Col>
+            <h3 className="fw-bold">LUFION Academy</h3>
+            <p className="desc">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatibus adipisci illum est sapiente. Itaque optio maiores
+              asperiores aspernatur quas expedita!
+            </p>
+            <div className="no mb-1 mt-4">
+              <Link className="text-decoration-none">
+                <i className="fa-brands fa-whatsapp"></i>
+                <p className="m-0">+62 885-7865-9936</p>
+              </Link>
+            </div>
+            <div className="email">
+              <Link className="text-decoration-none">
+                <i className="fa-regular fa-envelope"></i>
+                <p className="m-0">lufionacademy@gmail.com</p>
+              </Link>
+            </div>
+          </Col>
+          <Col></Col>
+          <Col></Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default FooterComponent;
